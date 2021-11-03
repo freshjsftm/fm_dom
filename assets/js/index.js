11274aee6dcbb56b094fc6dc87eb421bd99b31d2
Elena <freshjs.ftm@gmail.com>
2021-11-03 11:05:29 +0200

    Add btn delete

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,28 +1,43 @@
 'use strict';
-
-const messageArray = [];
-
+const state = [];
 const form = document.getElementById('root-form');
+const list = document.getElementById('list');
 
 form.addEventListener('submit', (e)=>{
   e.preventDefault();
-  const {target: { textInput }} = e;
-  const inputValue = textInput.value.trim();
-  //const validator = /(^[A-Z][a-z]{2,12} [A-Z][a-z]{1,17}$)|(^[А-Я][а-я]{2,12} [А-Я][а-я]{1,17}$)/;
-  //const validator = /^[А-Я][а-я]{1,17} ([А-Я]\.){2}$/;
-  
-  if(validator.test(inputValue)){
-    messageArray.push(inputValue);
+  const {target: { textInput: {value: inputText} }} = e;
+  const inputValue = inputText.trim();
+  const validatorName = /^[A-Z][a-z]{2,12}$/;
+  if(validatorName.test(inputValue) && !state.includes(inputValue)){
+    state.push(inputValue);
     form.reset();
+    const li = createElement('li', {classNames:['item']}, document.createTextNode(inputValue));
+    const btn = createElement('button', 
+      {typeEvent:'click', onClick:deleteHandler.bind(li), dataValue:inputValue}, 
+      document.createTextNode('X'));
+    li.append(btn);
+    list.append(li);
   }
-  //рендерить на странице то, что добавилось в массив
-  console.dir(messageArray);
+  console.dir(state);
 })
-
-
-function createElement(type,{classNames, typeEvent, onClick}, ...children){
+/*
+добавить кнопку удаления для каждого пункта списка
+по нажатию на кнопку удалять
+- пункт списка сос траницы
+- значение из массива
+*/
+function deleteHandler({target:{dataset:{value}}}){
+  this.remove();
+  state.splice(state.indexOf(value),1);
+}
+function createElement(type, {dataValue = '', classNames=[], typeEvent='', onClick=null}, ...children){
   const elem = document.createElement(type);
-  elem.classList.add(...classNames);
+  if(dataValue){
+    elem.dataset.value = dataValue;
+  }
+  if(classNames.length){
+    elem.classList.add(...classNames);
+  }
   elem.addEventListener(typeEvent, onClick);//for example
   elem.append(...children);
   return elem;
