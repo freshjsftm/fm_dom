dc19b0de91efdc61d991dc330dc0d985fca67a2f
Elena <freshjs.ftm@gmail.com>
2021-11-04 09:50:25 +0200

    Add JSON

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,26 @@
 'use strict';
 
-const btn = document.getElementById('btn');
-btn.addEventListener('click', btnHanler);
-function btnHanler(e){
-
+const user = {
+  name:'Elon',
+  sname:'Musk',
+  age:50,
+  getFullName(){
+    return `${this.name} ${this.sname}`;
+  },
+  children:['one', 'two'],
+  isAdult: true,
+  hasPet: undefined,
+  isUkrain: null,
+  [Symbol('test')]:123,
+  friends:{
+    friend1:'Tom',
+    friend2:'Tim',
+  },
 }
+console.log(user)
 
+const serializeUser = JSON.stringify(user);
+console.log(serializeUser)
 
-
-const identificator = setTimeout(()=>{
-  console.log('hi!')
-},300)
-
-clearTimeout(identificator);
-
-
-/*
-написать функцию, которая выводит последовательно числа 
-в консоль от 1 до 10 через 0,5 секунды
-*/
-const c1 = counter();
-function counter(){
-  let i=1;
-  return function(){
-    const id = setInterval(()=>{
-      console.log(i++)
-      if(i>10){
-        clearInterval(id);
-      }
-    }, 500)
-  }
-}
-c1();
+const deSerializeUser = JSON.parse(serializeUser)
+console.log(deSerializeUser)
