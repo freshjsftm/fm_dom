73389552edc6459d53eb1eea0b44bb601633d9a2
Elena <freshjs.ftm@gmail.com>
2021-11-04 11:46:11 +0200

    Add new Promise

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,5 @@
 'use strict';
-/*
-const promise = fetch('./assets/js/data.json');
-promise.then((response)=>{
-  const jsonPromise = response.json();
-  jsonPromise.then((data)=>{
-    console.table(data)
-  })
-})
-*/
+
 fetch('./assets/js/data.json')
   .then((response)=>response.json())
   .then((data)=>{
@@ -17,4 +9,28 @@ fetch('./assets/js/data.json')
     });
   })
   .catch((error)=>{ console.log(error);})
-  .finally(()=>{console.log('end');})
+  .finally(()=>{console.log('end');});
+
+
+  const myFirstPromise = new Promise(executor);
+  function executor(resolve, reject){
+    Math.random()>0.5?resolve('ok'):reject('lose');
+  }
+
+  myFirstPromise
+  .then((data)=>{console.log(data)})
+  .catch((err)=>{console.log(err)})
+
+  //setTimeout(callback, 200)
+  //timeout(ms).then(callback)
+
+  function timeout(ms){
+    return new Promise((resolve, reject)=>{
+      setTimeout(()=>{reject('error!!!!!!')}, ms)
+    })
+  }
+
+  timeout(1000)
+  .then(()=>{console.log(1)})
+  .catch((err)=>{console.log(err)})
+  .finally(()=>{console.log(3)})
