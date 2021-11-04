430d2cd36861d806c5ead9a4ad70c60f0b72417a
Elena <freshjs.ftm@gmail.com>
2021-11-04 12:14:18 +0200

    Add loadImage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,23 @@
 'use strict';
 
-fetch('./assets/js/data.json')
-  .then((response)=>response.json())
-  .then((data)=>{
-    //вывести в консоль только строковый параметр
-    data.forEach((user)=>{
-      console.log(user.name);
-    });
-  })
-  .catch((error)=>{ console.log(error);})
-  .finally(()=>{console.log('end');});
-
-
-  const myFirstPromise = new Promise(executor);
-  function executor(resolve, reject){
-    Math.random()>0.5?resolve('ok'):reject('lose');
-  }
-
-  myFirstPromise
-  .then((data)=>{console.log(data)})
-  .catch((err)=>{console.log(err)})
-
-  //setTimeout(callback, 200)
-  //timeout(ms).then(callback)
-
-  function timeout(ms){
-    return new Promise((resolve, reject)=>{
-      setTimeout(()=>{reject('error!!!!!!')}, ms)
+function loadImage(path){
+  const image = new Image();
+  image.src = path;
+  return new Promise((resolve, reject)=>{
+    image.addEventListener('load', ()=>{
+      resolve(image);
     })
-  }
+    image.addEventListener('error', ()=>{
+      reject(new Error('path invalid'))
+    })
+  })
+}
 
-  timeout(1000)
-  .then(()=>{console.log(1)})
-  .catch((err)=>{console.log(err)})
-  .finally(()=>{console.log(3)})
+
+loadImage('https://friendofthesea.org/public/news/blue-ocean-28668-2560x1600.jpg')
+.then((elem)=>{
+  document.body.prepend(elem);
+})
+.catch((err)=>{
+  console.log(err)
+})
